ace7cbe20d9e911b142629cd717eadc9ae10cd84
Roger Taracha <rojtaracha@gmail.com>
2019-05-10 17:39:46 +0300

    Using Images: [+] Using static images to display an inline logo & an image underneath the header.

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export class Header extends React.Component {
     
@@ -23,6 +23,10 @@ export class Header extends React.Component {
         let display = this.state.isLoggedIn ? 'Sample User' : this.props.message;
         return (
             <View style={styles.headStyle}>
+                <Image
+                style={styles.logoStyle}
+                source={ require('./img/logo.png')}
+                />
                 <Text
                     style={styles.headText}
                     onPress={this.toggleUser}>{display}
@@ -39,7 +43,8 @@ const styles = StyleSheet.create({
     headText: {
         textAlign: 'right',
         color: '#ffffff',
-        fontSize: 20
+        fontSize: 20,
+        flex: 1
     },
 
     // Style for our view that wraps our Text component
@@ -47,8 +52,16 @@ const styles = StyleSheet.create({
         paddingTop: 30,
         paddingRight: 10,
         backgroundColor: '#35605a',
-        flex: 1
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderColor: '#000000'
+    },
+
+    // Logo image style
+    logoStyle: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
     }
-
-
 });
--- app/views/Home.js
+++ app/views/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../sections/Header.js';
+import { Hero } from '../sections/Hero.js';
 
 export class Home extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <Header message = 'Press to Login' />
-                <Text style={{flex:8}}>This will be the Homepage</Text>
+                <Hero />
                 <Text style={{flex:6}}>These other lines are here</Text>
             </View>
         );
