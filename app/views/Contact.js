42c2e67c0601a66481dcf5db5b615a7f0cd34b41
Roger Taracha <rojtaracha@gmail.com>
2019-05-12 20:44:41 +0300

    Text Input:
    [+] Implementing Text Input using a form on the Contact page.

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, Alert } from 'react-native';
 import { Header } from '../sections/Header.js';
+import { StackNavigator } from 'react-navigation';
 
 export class Contact extends React.Component {
     
@@ -9,12 +10,67 @@ export class Contact extends React.Component {
         header: null // hides the navigation bar
     };
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            msg: 'Enter Message',
+            name: 'Enter Name',
+            email: 'Enter your Email Address'
+         }
+    }
+
+    // NOTE: With arrow functions, you can omit the curly brackets if you have 1 LOC.
+    // This method sets the fields to blank using setState.
+    clearFields=()=> this.setState({name:'', msg:'', email:''});
+
+    sendMessage=()=> {
+        Alert.alert(this.state.name, this.state.msg);
+        this.props.navigation.goBack(); // Navigate back to home page once form is submitted.
+    };
+
     render() {
         return (
             <View style={styles.container}>
                 <Header message='Press to Login' />
-                <Text style={{flex: 8}}>The Contact form will go here</Text>
-                <Text style={{flex: 6}}>More Contact form will go here</Text>
+                <Text style={styles.heading}>Contact Us</Text>
+
+                {/* Name Text Input */}
+                <TextInput 
+                    style={styles.inputs}
+                    onChangeText={(text) => this.setState({name: text})}
+                    value={this.state.name} 
+                />
+
+                {/* Message Text Input */}
+                <TextInput 
+                    style={styles.multiInput}
+                    onChangeText={(text) => this.setState({msg: text})}
+                    value={this.state.msg}
+                    multiline = {true}
+                    numberOfLines = {4} 
+                />
+
+                {/* Email Text Input */}
+                <TextInput 
+                    style={styles.inputs}
+                    onChangeText={(text) => this.setState({email: text})}
+                    value={this.state.email} 
+                />
+                
+                {/* Button to send our message */}
+                <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
+                    <Text style = {styles.buttons}>
+                        Send Message
+                    </Text>
+                </TouchableHighlight>
+
+                {/* Button to clear our fields */}
+                <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
+                    <Text style = {styles.buttons}>
+                        Reset Form
+                    </Text>
+                </TouchableHighlight>
+
             </View>
         );
     }
@@ -22,6 +78,26 @@ export class Contact extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingBottom: '45%' // Add padding to push inputs up the page to accommodate keyboard.
+    },
+    heading: {
+        fontSize: 16,
         flex: 1
+    },
+    inputs: {
+        flex: 1,
+        width: '80%',
+        padding: 10 // Added padding to get some spacing between the inputs
+    },
+    multiInput: {
+        flex: 2,
+        width: '90%',
+        paddingBottom: 20 // Added padding to give it some spacing from the input above it.
+    },
+    buttons: {
+        marginTop: 15, // Provides some spacing between the buttons.
+        fontSize: 16
     }
 });
